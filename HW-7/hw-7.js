17dacc1125c3640dafcfbc6046d8ec5a24480e78
Ferrol Brown <31551956+fbrown222@users.noreply.github.com>
2018-04-04 12:45:34 -0600

    first try

---
+++ HW-7/hw-7.js
@@ -0,0 +1,78 @@
+
+
+
+let body = document.body;
+let main_div = document.createElement("div");
+let text_stay_still = document.querySelector(".stay_still");
+let text_scream = document.querySelector(".scream");
+let text_investigate = document.querySelector(".investigate");
+let text_ignore = document.querySelector(".ignore");
+let text_make_a_run = document.querySelector(".make_a_run");
+let text_grab_cross = document.querySelector(".grab_cross");
+let text_throw_pic = document.querySelector(".throw_pic");
+let text_pic_up = document.querySelector(".pick_up");
+let text_window = document.querySelector(".window");
+let text_destroy = document.querySelector(".destroy");
+let text_scared = document.querySelector(".scared");
+let text_turn_in_friend = document.querySelector(".turn_in_friend");
+
+let bt_scream = document.querySelector('#bscream');
+let bt_stay_still = document.querySelector('#bstay_still');
+let bt_investigate = document.querySelector('#binvestigate');
+let bt_ignore = document.querySelector('#bignore');
+let bt_make_a_run = document.querySelector('#bmake-a-run');
+let bt_grab_cross = document.querySelector('#bgrab-cross');
+
+
+bt_scream.addEventListener("click", showHideText);
+bt_stay_still.addEventListener("click", showHideText);
+
+
+
+function showHideText() {
+    if(text_stay_still.hidden){
+        text_stay_still.hidden = false;
+        text_scream.hidden = true;
+        text_investigate.hidden= true;
+        text_ignore.hidden = true;
+        text_make_a_run.hidden = true;
+        text_grab_cross.hidden = true;
+        text_throw_pic.hidden = true;
+        text_window.hidden = true;
+        text_destroy.hidden = true;
+        text_scared.hidden = true;
+        text_turn_in_friend.hidden = true;
+
+
+        bt_scream.hidden=true;
+        bt_stay_still.hidden=true;
+        bt_investigate.hidden=false;
+        bt_ignore.hidden=false;
+        bt_make_a_run.hidden=true;
+        bt_grab_cross.hidden=true;
+
+        main_div.hidden=true;
+
+
+    } else {
+      text_stay_still.hidden = true;
+      text_scream.hidden = false;
+      text_investigate.hidden= true;
+      text_ignore.hidden = true;
+      text_make_a_run.hidden = true;
+      text_grab_cross.hidden = true;
+      text_throw_pic.hidden = true;
+      text_window.hidden = true;
+      text_destroy.hidden = true;
+      text_scared.hidden = true;
+      text_turn_in_friend.hidden = true;
+
+
+      bt_scream.hidden=true;
+      bt_stay_still.hidden=true;
+      bt_investigate.hidden=true;
+      bt_ignore.hidden=true;
+      bt_make_a_run.hidden=false;
+      bt_grab_cross.hidden=false;
+
+      main_div.hidden=true;
