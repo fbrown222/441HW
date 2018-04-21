eeed0e1d2127eb85ac33451cf02b83662ef242ce
Ferrol Brown <31551956+fbrown222@users.noreply.github.com>
2018-04-21 14:01:50 -0600

    test

--- HW-77/hw-77.js
+++ HW-77/hw-77.js
@@ -1,124 +1,49 @@
 
-let body = document.body;
-let noob = document.createElement("div");
-let bt_scream = document.querySelector('#bscream');
-let bt_stay = document.querySelector('#bstay');
-//let bt_retry = document.querySelector('#bretry');
-let text_intro = document.querySelector(".intro");
-let text_Scream = document.querySelector(".Scream");
-let text_Stay = document.querySelector(".Stay");
-let divEl1 = document.querySelector(".storageDiv");
-let formEl1 = document.querySelector(".nameForm");
+var app = angular.module('WikiApp', ['ngAnimate']);
+app.controller('MainCtrl', function($scope, $http, $timeout) {
+  var form = $('form');
+  var close = $('.eks');
+  var input = $('input');
+  var search = $("#search");
+  var help = $("#help");
 
+  $scope.results = [];
 
+  close.on('click', function() {
+    form.toggleClass('open');
 
-
-bt_scream.addEventListener("click", showHideText_Scream);
-bt_stay.addEventListener("click", showHideText_Stay);
-
-
-
-body.style.background = "#f7d8ff";
-
-
-
-
-function grabText() {
-    let textIn;
-    textIn = document.querySelector("#textIn1").value;
-
-    if( textIn.length < 1){ alert("Please refresh and enter a name! Anything! It can be fake!"); return }
-
-
-    let textToAdd = document.createTextNode("Hello "+textIn+", welcome to a very short story!");
-
-
-    let newP = document.createElement("P");
-
-
-    newP.appendChild(textToAdd);
-
-    let storage = document.querySelector("#storageDiv");
-
-    storage.appendChild(newP);
-
-}
-
-document.querySelector("#runButton").addEventListener( 'click', grabText);
-
-
-
-
-function showHideText_Scream() {
-    if(text_Scream.hidden){
-        bt_scream.hidden= true;
-        text_intro.hidden = false;
-        formEl1.hidden = true;
-        text_Scream.hidden = false;
-        text_Stay.hidden= true;
-        bt_stay.hidden=true;
-      //  bt_retry.hidden=false;
-        noob.hidden=true;
-
-        body.style.background = "#71aff2";
-
-function alertPop1(){
-        alert("OH NOOOOOO!!!!!");
-}
-
-setTimeout( alertPop1, 500 );
-
-
-
-    } else {
-      bt_scream.hidden =false
-      text_intro.hidden = false;
-      formEl1.hidden = false;
-      text_Scream.hidden = true;
-      text_Stay.hidden= true;
-      bt_stay.hidden=false;
-    //  bt_retry.hidden=true;
-      noob.hidden=false;
-
-
+    if (!form.hasClass('open') && $scope.searchTxt !== '' && typeof $scope.searchTxt !== 'undefined') {
+	    search.toggleClass('fullHeight')
+      help.toggleClass('hide');
+      $scope.searchTxt = '';
     }
+    $scope.results = [];
+    $scope.$apply();
+  })
 
-}
-
-
-function showHideText_Stay() {
-    if(text_Stay.hidden){
-        bt_stay.hidden = true;
-        text_intro.hidden = false;
-        formEl1.hidden = true;
-        text_Scream.hidden = true;
-        text_Stay.hidden= false;
-        bt_scream.hidden=true;
-      //  bt_retry.hidden=false;
-        noob.hidden=true;
-
-        body.style.background = "#b170f1";
-
-        function alertPop2(){
-                alert("awww yissss!!!!!");
-        }
-
-        setTimeout( alertPop2, 500 );
-
-
+  input.on('transitionend webkitTransitionEnd oTransitionEnd', function() {
+    if (form.hasClass('open')) {
+      input.focus();
     } else {
-      bt_stay.hidden = false;
-      text_intro.hidden = false;
-      formEl1.hidden = false;
-      text_Scream.hidden = true;
-      text_Stay.hidden= true;
-      bt_scream.hidden=false;
-    //  bt_retry.hidden=true;
-      noob.hidden=false;
-
-
+      return;
     }
+  })
 
-}
+  $scope.search = function() {
+    $scope.results = [];
+    help.addClass('hide');
+    search.removeClass('fullHeight');
+    var title = input.val();
+    var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+    var cb = '&callback=JSON_CALLBACK';
+    var page = 'https://en.wikipedia.org/?curid=';
 
-body.appendChild(noob);
+    $http.jsonp(api + title + cb)
+    .success(function(data) {
+      var results = data.query.pages;
+      angular.forEach(results, function(v,k)  {
+        $scope.results.push({title: v.title, body: v.extract, page: page + v.pageid})
+      })
+    });
+  }
+});
