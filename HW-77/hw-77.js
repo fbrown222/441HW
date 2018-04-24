f22601dccbbd34d64f28382a078b9ac7f32878ee
Ferrol Brown <31551956+fbrown222@users.noreply.github.com>
2018-04-23 19:39:35 -0600

    leave alt and lat

--- HW-77/hw-77.js
+++ HW-77/hw-77.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
       var lat = position.coords.latitude;
       var longit = position.coords.longitude;
       var altitude = position.coords.altitude;
-      //var latitude_text = document.getElementById("latitude-val");
-    //  var altitude_text = document.getElementById("altit");
+      var latitude_text = document.getElementById("latitude-val");
+      var altitude_text = document.getElementById("altit");
       var city_name;
       var temp;
       var pressure;
@@ -18,8 +18,8 @@ $(document).ready(function() {
       var weather_description;
       var apiKey = "699b6e6504a91f6e2ac509b14902c8a1";
 
-      //altitude_text.innerHTML = "Altitude is " + altitude;
-    //  latitude_text.innerHTML = "Latitude is " + lat;
+      altitude_text.innerHTML = "Altitude is " + altitude;
+      latitude_text.innerHTML = "Latitude is " + lat;
 
       $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longit +"&units=imperial"+ "&appid=" + apiKey, function(data) {
 
