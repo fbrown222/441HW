c644e07524ec95730d2b04983ba8912b1b951410
Ferrol Brown <31551956+fbrown222@users.noreply.github.com>
2018-04-23 19:32:54 -0600

    unit

--- HW-77/hw-77.js
+++ HW-77/hw-77.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
       var lat = position.coords.latitude;
       var longit = position.coords.longitude;
       var altitude = position.coords.altitude;
-      var latitude_text = document.getElementById("latitude-val");
-      var altitude_text = document.getElementById("altit");
+      //var latitude_text = document.getElementById("latitude-val");
+    //  var altitude_text = document.getElementById("altit");
       var city_name;
       var temp;
       var pressure;
@@ -18,15 +18,15 @@ $(document).ready(function() {
       var weather_description;
       var apiKey = "699b6e6504a91f6e2ac509b14902c8a1";
 
-      altitude_text.innerHTML = "Altitude is " + altitude;
-      latitude_text.innerHTML = "Latitude is " + lat;
+      //altitude_text.innerHTML = "Altitude is " + altitude;
+    //  latitude_text.innerHTML = "Latitude is " + lat;
 
-      $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longit + "&appid=" + apiKey, function(data) {
+      $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longit +"&units=imperial"+ "&appid=" + apiKey, function(data) {
 
         city_name = data["name"];
         country_name = data["sys"]["country"];
         weather_description = data["weather"][0]["description"];
-        temp = data["main"]["temp"]["imperial"];
+        temp = data["main"]["temp"];
         pressure = data["main"]["pressure"];
         wind_speed = data["wind"]["speed"];
 
